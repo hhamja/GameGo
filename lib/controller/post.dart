04b0d1eb685207d4f02e689fc61ce6668087b408
CodeKinggggggg <kmsaa@naver.com>
2022-09-08 03:14:33 +0900

    09-08 15:14

--- lib/controller/post.dart
+++ lib/controller/post.dart
@@ -1,17 +1,33 @@
 import 'package:mannergamer/utilites/index.dart';
 
 class PostController extends GetxController {
-  /* Initialize Firestore Instance */
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  // /* Initialize Firestore Instance */
+  // FirebaseFirestore firestore = FirebaseFirestore.instance;
+  /* 파이어스토어 Post 컬렉션 참조 instance */
+  final CollectionReference _post =
+      FirebaseFirestore.instance.collection('posts');
+
+  /* Put -> 홈페이지 드랍다운버튼 컨트롤러 */
+  HomePageDropDownBTController _ = Get.find<HomePageDropDownBTController>();
+
   /* RxList postList [] 선언 */
   RxList<PostModel> postList = <PostModel>[].obs;
+
+  /* 게시물 필터 결과값 담을 resultList 선언 */
+  RxList<PostModel> resultList = <PostModel>[].obs;
 
   /* Lifecycle */
   @override
   void onInit() {
     super.onInit();
     postList.bindStream(readPostData());
-    print(postList);
+    resultList.bindStream(
+      streamPostData(
+        _.selectedModeValue,
+        _.selectedPositionValue,
+        _.selectedTearValue,
+      ),
+    );
   }
 
   @override
@@ -22,7 +38,7 @@ class PostController extends GetxController {
   /* Create Post */
   Future createPost(PostModel postModel) async {
     try {
-      final res = await firestore.collection('post').add({
+      final res = await _post.add({
         'username': postModel.username,
         'title': postModel.title,
         'maintext': postModel.maintext,
@@ -40,8 +56,7 @@ class PostController extends GetxController {
 
   /* Stream Read Post */
   Stream<List<PostModel>> readPostData() {
-    return firestore
-        .collection('post')
+    return _post
         .orderBy('createdAt', descending: true)
         .snapshots()
         .map((QuerySnapshot query) {
@@ -54,6 +69,190 @@ class PostController extends GetxController {
     });
   }
 
+  /* Stream Read Post */
+  Stream<List<PostModel>> gamemodeFilter(
+      String gamemode, String? position, String? tear) {
+    filter(List gamemode, index) {
+      List gamemode = gameModes;
+      _post
+          .orderBy('createdAt', descending: true)
+          .where('gamemode', isEqualTo: gamemode)
+          .snapshots()
+          .map((snapshot) => snapshot.docs.map((e) {
+                return PostModel.fromDocumentSnapshot(e);
+              }).toList());
+    }
+
+    switch (gamemode) {
+      case '솔로랭크':
+        resultList.clear();
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('gamemode', isEqualTo: '솔로랭크')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+
+        break;
+      case '자유랭크':
+        resultList.clear();
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('gamemode', isEqualTo: gameModes[0])
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+
+        break;
+      case '일반게임':
+        resultList.clear();
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('gamemode', isEqualTo: '일반게임')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+
+        break;
+      case '무작위 총력전':
+        resultList.clear();
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('gamemode', isEqualTo: '무작위 총력전')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+      case 'AI 대전':
+        resultList.clear();
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('gamemode', isEqualTo: 'AI 대전')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+    }
+    switch (position) {
+      case '탑':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('position', isEqualTo: 'AI 대전')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+      case '정글':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('position', isEqualTo: '정글')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+      case '미드':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('position', isEqualTo: '미드')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+      case '원딜':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('position', isEqualTo: '원딜')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+      case '서포터':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('position', isEqualTo: '서포터')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+    }
+    switch (tear) {
+      case '언랭크':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('tear', isEqualTo: '언랭크')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+      case '아이언':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('tear', isEqualTo: '아이언')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+      case '브론즈':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('tear', isEqualTo: '브론즈')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+      case '실버':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('tear', isEqualTo: '실버')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+      case '골드':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('tear', isEqualTo: '골드')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+      case '플래티넘':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('tear', isEqualTo: '플래티넘')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+      case '다이아몬드':
+        _post
+            .orderBy('createdAt', descending: true)
+            .where('tear', isEqualTo: '다이아몬드')
+            .snapshots()
+            .map((snapshot) => snapshot.docs.map((e) {
+                  return PostModel.fromDocumentSnapshot(e);
+                }).toList());
+        break;
+    }
+    throw [];
+  }
+
   /* Update Post, edit post*/
   Future updatePost(
     postid,
@@ -64,7 +263,7 @@ class PostController extends GetxController {
     String? tear,
   ) async {
     try {
-      await firestore.collection('post').doc(postid).update({
+      await _post.doc(postid).update({
         'title': title,
         'maintext': maintext,
         'gamemode': gamemode,
@@ -79,7 +278,7 @@ class PostController extends GetxController {
   /* Delete Post () */
   Future deletePost(postid) async {
     try {
-      final data = await firestore.collection('post').doc(postid).delete();
+      final data = await _post.doc(postid).delete();
       return data;
     } catch (e) {
       print('deletePost error : ${e}');
