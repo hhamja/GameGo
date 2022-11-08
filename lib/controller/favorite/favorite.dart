fe5029119aef6715a81f87652a196a7b0e44d0a0
CodeKinggggggg <kmsaa@naver.com>
2022-11-08 18:28:32 +0900

    Add - 나의정보 페이지의 관심게시글 UI
    ##DESC
    - 없었던 나의 관심 게시글 UI추가
    아직 로직을 연결한 세부적인 페이지 디테일은 구현 X

--- lib/controller/favorite/favorite.dart
+++ lib/controller/favorite/favorite.dart
@@ -5,6 +5,9 @@ class FavoriteController extends GetxController {
   /* 파이어스토어 user 컬렉션 참조 instance */
   final CollectionReference _userDB =
       FirebaseFirestore.instance.collection('user');
+  /* 파이어스토어 post 컬렉션 참조 instance */
+  final CollectionReference _postDB =
+      FirebaseFirestore.instance.collection('user');
   /* 게시물 좋아요 버튼 클릭하면 on/off 되는 bool 값 */
   RxBool isFavorite = false.obs;
   /* 나의 관심 게시물 리스트 */
@@ -60,13 +63,16 @@ class FavoriteController extends GetxController {
   }
 
   /* DB에서 관심 게시글 받기 */
-  Future getFavoriteList(uid, postId) async {
+  Future getFavoriteList(uid) async {
     List postIdList = [];
     final ref = _userDB.doc(uid).collection('favorite');
-    await ref
-        .orderBy('updatedAt', descending: true)
-        .get()
-        .then((snapshot) => postIdList.assignAll(snapshot.docs));
+    await ref.orderBy('updatedAt', descending: true).get().then(
+        (value) => postIdList.assignAll(value.docs.map((e) => e.reference.id)));
     print(postIdList);
+    for (var id in postIdList) {
+      final doc = _postDB.doc(id).get();
+      print(doc);
+    }
   }
 }
+  
--- lib/screens/my_info/my_activity/my_favorite/my_favorite.dart
+++ lib/screens/my_info/my_activity/my_favorite/my_favorite.dart
@@ -1,112 +1,131 @@
 import 'package:mannergamer/utilites/index/index.dart';
 
-class MyFavoriteList extends StatelessWidget {
+class MyFavoriteList extends StatefulWidget {
   MyFavoriteList({Key? key}) : super(key: key);
 
+  @override
+  State<MyFavoriteList> createState() => _MyFavoriteListState();
+}
+
+class _MyFavoriteListState extends State<MyFavoriteList> {
   final FavoriteController _favorite = Get.put(FavoriteController());
+  final currentUid = FirebaseAuth.instance.currentUser!.uid;
+
+  @override
+  void initState() {
+    super.initState();
+    _favorite.getFavoriteList(currentUid);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => RefreshIndicator(
-        //새로고침 시 PostList의 바뀐 값을 반영하여 Ui에 업데이트함
-        onRefresh: () async {},
-        displacement: 0, //맨 위에 위치시키는 값
-        child: ListView.separated(
-          physics: AlwaysScrollableScrollPhysics(), //리스트가 적어도 스크롤 인식 가능
-          padding: EdgeInsets.only(top: 10),
-          separatorBuilder: (BuildContext context, int index) {
-            return Divider(
-              thickness: 1,
-            );
-          },
-          itemCount: _favorite.favoriteList.length,
-          itemBuilder: (BuildContext context, int index) {
-            String time =
-                Jiffy(_favorite.favoriteList[index].createdAt.toDate())
-                    .fromNow();
-            return ListTile(
-              onTap: () {
-                Get.toNamed('/postdetail', arguments: {
-                  'index': index,
-                });
-              },
-              title: Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('관심 게시글'),
+        centerTitle: true,
+      ), 
+      body: Obx(
+        () => RefreshIndicator(
+          //새로고침 시 PostList의 바뀐 값을 반영하여 Ui에 업데이트함
+          onRefresh: () async {},
+          displacement: 0, //맨 위에 위치시키는 값
+          child: ListView.separated(
+            physics: AlwaysScrollableScrollPhysics(), //리스트가 적어도 스크롤 인식 가능
+            padding: EdgeInsets.only(top: 10),
+            separatorBuilder: (BuildContext context, int index) {
+              return Divider(
+                thickness: 1,
+              );
+            },
+            itemCount: _favorite.favoriteList.length,
+            itemBuilder: (BuildContext context, int index) {
+              String time =
+                  Jiffy(_favorite.favoriteList[index].createdAt.toDate())
+                      .fromNow();
+              return ListTile(
+                onTap: () {
+                  Get.toNamed('/postdetail', arguments: {
+                    'index': index,
+                  });
+                },
+                title: Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          /* 프로필 */
+                          CircleAvatar(
+                            radius: 10,
+                            backgroundImage: NetworkImage(
+                                _favorite.favoriteList[index].profileUrl),
+                          ),
+                          SizedBox(width: 5),
+                          /* 유저이름 */
+                          Text(
+                            _favorite.favoriteList[index].userName,
+                            style: TextStyle(height: 1.2),
+                          ),
+                          /* 날짜 */
+                          Expanded(
+                            child: Text(
+                              time,
+                              style: TextStyle(height: 1.2, fontSize: 10),
+                              textAlign: TextAlign.end,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      /* 게시글 제목 */
+                      Text(_favorite.favoriteList[index].title, maxLines: 1),
+                    ],
+                  ),
+                ),
+                subtitle: Column(
                   children: [
                     Row(
                       children: [
-                        /* 프로필 */
-                        CircleAvatar(
-                          radius: 10,
-                          backgroundImage: NetworkImage(
-                              _favorite.favoriteList[index].profileUrl),
-                        ),
-                        SizedBox(width: 5),
-                        /* 유저이름 */
-                        Text(
-                          _favorite.favoriteList[index].userName,
-                          style: TextStyle(height: 1.2),
-                        ),
-                        /* 날짜 */
-                        Expanded(
-                          child: Text(
-                            time,
-                            style: TextStyle(height: 1.2, fontSize: 10),
-                            textAlign: TextAlign.end,
-                          ),
+                        SizedBox(
+                          width: double.minPositive,
                         ),
                       ],
                     ),
-                    SizedBox(height: 10),
-                    /* 게시글 제목 */
-                    Text(_favorite.favoriteList[index].title, maxLines: 1),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        /* 게임모드 · 포지션 · 티어 */
+                        Text(
+                          _favorite.favoriteList[index].gamemode,
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          _favorite.favoriteList[index].position != null
+                              ? ' · ${_favorite.favoriteList[index].position}'
+                              : '',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          _favorite.favoriteList[index].tear != null
+                              ? ' · ${_favorite.favoriteList[index].tear}'
+                              : '',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        // Expanded(child: SizedBox()),
+                        // /* 체팅 수 */
+                        // Icon(Icons.chat_bubble_outline, size: 15),
+                        // Text('1'),
+                        // /* 좋아요 수 */
+                        // Icon(Icons.favorite_border_outlined, size: 15),
+                        // Text('1'),
+                      ],
+                    ),
                   ],
                 ),
-              ),
-              subtitle: Column(
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: double.minPositive,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      /* 게임모드 · 포지션 · 티어 */
-                      Text(
-                        _favorite.favoriteList[index].gamemode,
-                        style: TextStyle(fontSize: 12),
-                      ),
-                      Text(
-                        _favorite.favoriteList[index].position != null
-                            ? ' · ${_favorite.favoriteList[index].position}'
-                            : '',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                      Text(
-                        _favorite.favoriteList[index].tear != null
-                            ? ' · ${_favorite.favoriteList[index].tear}'
-                            : '',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                      // Expanded(child: SizedBox()),
-                      // /* 체팅 수 */
-                      // Icon(Icons.chat_bubble_outline, size: 15),
-                      // Text('1'),
-                      // /* 좋아요 수 */
-                      // Icon(Icons.favorite_border_outlined, size: 15),
-                      // Text('1'),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
