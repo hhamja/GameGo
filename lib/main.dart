abad5967810b8db678288f5b2bbeb732e3afd3b3
CodeKinggggggg <kmsaa@naver.com>
2022-11-25 00:11:04 +0900

    Add - FCM
    ##DESC
    -Pub Get

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:mannergamer/utilites/index/index.dart';
 import 'firebase_options.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  /* 앱이 백그라운드 상태일때 수신되는 메세지를 처리하는 부분 */
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await Jiffy.locale('ko'); //시간표시 한국어로 변환
 
   Get.put(InitialScreenCntroller());
