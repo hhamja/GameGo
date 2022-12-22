9229ac41be83c8d526a34d45bfeb4466232e3c0e
CodeKinggggggg <kmsaa@naver.com>
2022-12-22 19:57:31 +0900

    Remove - 채팅방 각각의 알림on/off  UI
    ##DESC
    : mvp에서 채팅방 따로따로의 알림 on/off 기능은 불필요하다고 생각되어서 주석처리함
    : 채팅방리스트에서 슬라이더로 나타나는 알림 아이콘, 메시지 페이지에서 앱바 오른쪽의 점3개 클릭 시 나타나는 바텀시트 목록 중 '알람켜기/끄기'

--- lib/view/chat/bottomSheet/chat_bottomSheet.dart
+++ lib/view/chat/bottomSheet/chat_bottomSheet.dart
@@ -18,14 +18,14 @@ class _ChatBottomSheetState extends State<ChatBottomSheet> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      height: 180, //개당 60
+      height: 120, //개당 60
       child: Column(
         //알림끄기, 차단, 신고, 나가기(red), 취소
         children: [
-          CustomButtomSheet(_isAlramOnOff ? '알림끄기' : '알림켜기', Colors.blue, () {
-            setState(() => _isAlramOnOff = !_isAlramOnOff); //토글버튼
-            Get.back();
-          }),
+          // CustomButtomSheet(_isAlramOnOff ? '알림끄기' : '알림켜기', Colors.blue, () {
+          //   setState(() => _isAlramOnOff = !_isAlramOnOff); //토글버튼
+          //   Get.back();
+          // }),
           // CustomButtomSheet(!_isBlockOnOff ? '차단하기' : '차단 해제하기', Colors.blue,
           //     () {
           //   setState(() => _isBlockOnOff = !_isBlockOnOff); //토글버튼
--- lib/view/chat/chat_list/chat_list.dart
+++ lib/view/chat/chat_list/chat_list.dart
@@ -55,93 +55,94 @@ class _ChatListPageState extends State<ChatListPage> {
                 _chatList.contactUser[0] != CurrentUser.uid) {
               _contactUser = _chatList.contactUser;
             }
-            return Slidable(
-              endActionPane: ActionPane(
-                extentRatio: 0.2, //한개당 0.2, 삭제버튼 추가시 0.4로 수정할 것
-                motion: DrawerMotion(),
+            return
+                // Slidable(
+                //   endActionPane: ActionPane(
+                //     extentRatio: 0.2, //한개당 0.2, 삭제버튼 추가시 0.4로 수정할 것
+                //     motion: DrawerMotion(),
+                //     children: [
+                //       /* 알림 on/off */
+                //       SlidableAction(
+                //         backgroundColor: Colors.grey,
+                //         foregroundColor: Colors.white,
+                //         icon: (_click == true)
+                //             ? CupertinoIcons.bell_solid
+                //             : CupertinoIcons.bell_slash_fill,
+                //         onPressed: (_) {
+                //           setState(() {
+                //             _click = !_click;
+                //           });
+                //         },
+                //       ),
+                //       // /* 채팅 나가기 (DB에서 삭제 X)*/
+                //       // SlidableAction(
+                //       //   onPressed: (_) async {
+                //       //     await _chat.chatRoomList.removeAt(index);
+                //       //     await _chat.deleteChat(_chatList.chatRoomId);
+                //       //   },
+                //       //   backgroundColor: Color(0xFFFE4A49),
+                //       //   foregroundColor: Colors.white,
+                //       //   icon: Icons.delete,
+                //       // ),
+                //     ],
+                //   ),
+                //   child:
+                ListTile(
+              /* 상대 유저 프로필 사진 */
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(_contactUser[1]),
+              ),
+              /* 이름 · 시간 */
+              title: Text(
+                _contactUser[2],
+                maxLines: 1,
+              ),
+              /* 마지막 대화 내용 */
+              subtitle: Text(
+                _chatList.lastContent,
+                maxLines: 1,
+                softWrap: true,
+                overflow: TextOverflow.ellipsis,
+              ),
+              /* 최근시간 · 읽지 않은 메시지 수 */
+              trailing: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  /* 알림 on/off */
-                  SlidableAction(
-                    backgroundColor: Colors.grey,
-                    foregroundColor: Colors.white,
-                    icon: (_click == true)
-                        ? CupertinoIcons.bell_solid
-                        : CupertinoIcons.bell_slash_fill,
-                    onPressed: (_) {
-                      setState(() {
-                        _click = !_click;
-                      });
-                    },
+                  Text(
+                    _time,
+                    style: TextStyle(fontSize: 10),
                   ),
-                  // /* 채팅 나가기 (DB에서 삭제 X)*/
-                  // SlidableAction(
-                  //   onPressed: (_) async {
-                  //     await _chat.chatRoomList.removeAt(index);
-                  //     await _chat.deleteChat(_chatList.chatRoomId);
-                  //   },
-                  //   backgroundColor: Color(0xFFFE4A49),
-                  //   foregroundColor: Colors.white,
-                  //   icon: Icons.delete,
-                  // ),
+                  /* 안읽은 메시지 수
+                      * 0개가 아닐때만 표시 */
+                  _chatList.unReadCount['${CurrentUser.uid}'] != 0
+                      ? CircleAvatar(
+                          backgroundColor: Colors.red,
+                          radius: 10,
+                          child: Text(
+                            /* 나의 uid에 해당하는 안읽은 메시지 수 받음 */
+                            _chatList.unReadCount['${CurrentUser.uid}']
+                                .toString(),
+                            style: TextStyle(fontSize: 12, color: Colors.white),
+                          ),
+                        )
+                      : SizedBox.shrink(), // 읽지 않은 메시지 알려주는 빨간숫자
                 ],
               ),
-              child: ListTile(
-                /* 상대 유저 프로필 사진 */
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(_contactUser[1]),
-                ),
-                /* 이름 · 시간 */
-                title: Text(
-                  _contactUser[2],
-                  maxLines: 1,
-                ),
-                /* 마지막 대화 내용 */
-                subtitle: Text(
-                  _chatList.lastContent,
-                  maxLines: 1,
-                  softWrap: true,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                /* 최근시간 · 읽지 않은 메시지 수 */
-                trailing: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      _time,
-                      style: TextStyle(fontSize: 10),
-                    ),
-                    /* 안읽은 메시지 수
-                      * 0개가 아닐때만 표시 */
-                    _chatList.unReadCount['${CurrentUser.uid}'] != 0
-                        ? CircleAvatar(
-                            backgroundColor: Colors.red,
-                            radius: 10,
-                            child: Text(
-                              /* 나의 uid에 해당하는 안읽은 메시지 수 받음 */
-                              _chatList.unReadCount['${CurrentUser.uid}']
-                                  .toString(),
-                              style:
-                                  TextStyle(fontSize: 12, color: Colors.white),
-                            ),
-                          )
-                        : SizedBox.shrink(), // 읽지 않은 메시지 알려주는 빨간숫자
-                  ],
-                ),
-                onTap: () {
-                  Get.toNamed(
-                    '/chatscreen',
-                    arguments: {
-                      'chatRoomId': _chatList.chatRoomId,
-                      'postId': _chatList.postId,
-                      'uid': _contactUser[0], //상대유저 uid
-                      'profileUrl': _contactUser[1], //상대유저 프로필
-                      'userName': _contactUser[2], //상대유저 이름
-                      'mannerAge': _contactUser[3], //상대유저 매너나이
-                    }, //상대유저정보 전달
-                  );
-                },
-              ),
+              onTap: () {
+                Get.toNamed(
+                  '/chatscreen',
+                  arguments: {
+                    'chatRoomId': _chatList.chatRoomId,
+                    'postId': _chatList.postId,
+                    'uid': _contactUser[0], //상대유저 uid
+                    'profileUrl': _contactUser[1], //상대유저 프로필
+                    'userName': _contactUser[2], //상대유저 이름
+                    'mannerAge': _contactUser[3], //상대유저 매너나이
+                  }, //상대유저정보 전달
+                );
+              },
+              // ),
             );
           },
         ),
