702993563a3cab423eafd75f96ea6aefd867e244
CodeKinggggggg <kmsaa@naver.com>
2022-12-28 15:32:40 +0900

    Add - CurrentUser 클래스
    ##DESC
    - 추가 사항 -
    1. auth에서 현재유저의 이름을 static으로 'name' 추가
    2. auth에서 현재유저의 프로필 url을 static으로 'profile' 추가

--- lib/utilites/currentUser/currentUser.dart
+++ lib/utilites/currentUser/currentUser.dart
@@ -2,4 +2,7 @@ import 'package:mannergamer/utilites/index/index.dart';
 
 class CurrentUser {
   static final String uid = FirebaseAuth.instance.currentUser!.uid;
+  static final String name =
+      FirebaseAuth.instance.currentUser!.displayName ?? '(이름없음)';
+  static final String profile = FirebaseAuth.instance.currentUser!.photoURL!;
 }
