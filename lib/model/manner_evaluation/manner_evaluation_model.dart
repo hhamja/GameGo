2573e4b9dd71c733ac7f936c324bfa740912be0d
CodeKinggggggg <kmsaa@naver.com>
2023-01-05 18:28:18 +0900

    Add - 매너평가 항목에 대한 enum
    ##DESC
    1 enum BadEvaluation {
      badManner, //불친절하고 매너가 나빠요.
      appointment, //시간 약속을 안 지켜요
      answer, //응답이 늦어요
      mental, //맨탈이 약해요
      gameSkill, //게임 실력이 아쉬워요
      troll, //고의적으로 트롤 행위를 해요
      abuse, //욕설이나 험악한 말을 해요
      sexual, //성적인 발언을 해요
      shortTalk, //반말을 사용해요
      communication, //소통을 안해요
      uncomfortable, //불편한 분위기를 만들어요
      privateMeeting, //사적인 만남을 하려고 해요.
    }
    
    2. enum GoodEvaluation {
      kindManner, //친정하고 매너가 좋아요.
      appointment, //시간 약속을 잘 지켜요.
      answer, //응답이 빨라요.
      mental, //맨탈이 강해요.
      gameSkill, //게임 실력이 뛰어나요.
      comfortable, //불편하지 않게 편하게 대해줘요.
      speaking, //착하고 부드럽게 말해요.
      Communication, //게임할 때 소통을 잘해요.
      hardGame, //게임을 진심으로 열심히 해요.
    }

--- lib/model/manner_evaluation/manner_evaluation_model.dart
+++ lib/model/manner_evaluation/manner_evaluation_model.dart
@@ -27,58 +27,58 @@ class MannerEvaluationModel {
   }
 }
 
-// //9개 매너 평가 항목 리스트
-// var goodMannerList = [
-//   '친절하고 매너가 좋아요.',
-//   '시간 약속을 잘 지켜요.',
-//   '응답이 빨라요.',
-//   '맨탈이 강해요.',
-//   '게임 실력이 뛰어나요.',
-//   '불편하지 않게 편하게 대해줘요.',
-//   '착하고 부드럽게 말해요.',
-//   '게임할 떄 소통을 잘해요.',
-//   '게임을 진심으로 열심히 해요.'
-// ];
+//9개 매너 평가 항목 리스트
+var goodMannerList = [
+  '친절하고 매너가 좋아요.',
+  '시간 약속을 잘 지켜요.',
+  '응답이 빨라요.',
+  '맨탈이 강해요.',
+  '게임 실력이 뛰어나요.',
+  '불편하지 않게 편하게 대해줘요.',
+  '착하고 부드럽게 말해요.',
+  '게임할 떄 소통을 잘해요.',
+  '게임을 진심으로 열심히 해요.'
+];
 
-// //12개 비매너 평가 항목 리스트
-// var badMannerList = [
-//   '불친절하고 매너가 나빠요.',
-//   '시간 약속을 안 지켜요.',
-//   '응답이 늦어요.',
-//   '맨탈이 약해요.',
-//   '게임 실력이 아쉬워요.',
-//   '고의적으로 트롤 행위를 해요.',
-//   '욕설이나 험악한 말을 해요',
-//   '성적인 발언을 해요.',
-//   '반말을 사용해요',
-//   '소통을 안해요.',
-//   '불편한 분위기를 만들어요.',
-//   '사적인 만남을 하려고 해요.',
-// ];
+//12개 비매너 평가 항목 리스트
+var badMannerList = [
+  '불친절하고 매너가 나빠요.',
+  '시간 약속을 안 지켜요.',
+  '응답이 늦어요.',
+  '맨탈이 약해요.',
+  '게임 실력이 아쉬워요.',
+  '고의적으로 트롤 행위를 해요.',
+  '욕설이나 험악한 말을 해요',
+  '성적인 발언을 해요.',
+  '반말을 사용해요',
+  '소통을 안해요.',
+  '불편한 분위기를 만들어요.',
+  '사적인 만남을 하려고 해요.',
+];
 
-// enum BadEvaluation {
-//   badManner, //불친절하고 매너가 나빠요.
-//   appointment, //시간 약속을 안 지켜요
-//   answer, //응답이 늦어요
-//   mental, //맨탈이 약해요
-//   gameSkill, //게임 실력이 아쉬워요
-//   troll, //고의적으로 트롤 행위를 해요
-//   abuse, //욕설이나 험악한 말을 해요
-//   sexual, //성적인 발언을 해요
-//   shortTalk, //반말을 사용해요
-//   communication, //소통을 안해요
-//   uncomfortable, //불편한 분위기를 만들어요
-//   privateMeeting, //사적인 만남을 하려고 해요.
-// }
+enum BadEvaluation {
+  badManner, //불친절하고 매너가 나빠요.
+  appointment, //시간 약속을 안 지켜요
+  answer, //응답이 늦어요
+  mental, //맨탈이 약해요
+  gameSkill, //게임 실력이 아쉬워요
+  troll, //고의적으로 트롤 행위를 해요
+  abuse, //욕설이나 험악한 말을 해요
+  sexual, //성적인 발언을 해요
+  shortTalk, //반말을 사용해요
+  communication, //소통을 안해요
+  uncomfortable, //불편한 분위기를 만들어요
+  privateMeeting, //사적인 만남을 하려고 해요.
+}
 
-// enum GoodEvaluation {
-//   kindManner,
-//   appointment,
-//   answer,
-//   mental,
-//   gameSkill,
-//   comfortable,
-//   speaking,
-//   Communication,
-//   hardGame,
-// }
+enum GoodEvaluation {
+  kindManner,
+  appointment,
+  answer,
+  mental,
+  gameSkill,
+  comfortable,
+  speaking,
+  Communication,
+  hardGame,
+}
