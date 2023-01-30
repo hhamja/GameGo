1473581297c5de0f7e8ae73007f70aedc9573094
CodeKinggggggg <kmsaa@naver.com>
2023-01-30 14:42:11 +0900

    Edit - 유저 로그아웃 로직
    ##DECS
    1. 수정 사항
    : 로그아웃시 서버의 유저 토큰 값 데이터를 null로 업데이트
    
    2.  수정 이유
    : 유저가 로그아웃을 하는 경우, 앱의 알림이 수신되면 안되기 때문에 토큰 값을 null로 수정하는 것

--- lib/controller/user/user.dart
+++ lib/controller/user/user.dart
@@ -21,7 +21,7 @@ class UserController extends GetxController {
   String verificationID = '';
   int? _resendToken;
 
-  /* 유저 생성하기 */
+  // 유저 생성하기
   Future addNewUser(UserModel userModel) async {
     await _userDB.doc(userModel.uid).set(
       {
@@ -41,7 +41,7 @@ class UserController extends GetxController {
     );
   }
 
-  /* 유저가 닉네임 변경 */
+  // 유저가 닉네임 변경
   Future updateUserName(userName) async {
     // 닉네임 중복확인
     // 탈퇴 플래그 처리한 유저 닉네임도 고려할 것이므로 플래그 쿼리 X
@@ -181,16 +181,16 @@ class UserController extends GetxController {
     });
   }
 
-  /// 나의 프로필을 변경하기
+  // 나의 프로필을 변경하기
   Future updateUserProfile(profileUrl) async {
-    /// 유저정보에서 프로필 수정
+    // 유저정보에서 프로필 수정
     _userDB.doc(CurrentUser.uid).update(
       {
         'profileUrl': profileUrl,
       },
     );
 
-    /// 채팅에서 프로필 수정
+    // 채팅에서 프로필 수정
     _chatDB.where('members', arrayContains: CurrentUser.uid).get().then(
       // 내가 맴버로 있는 채팅방만 쿼리하여 리스트로 받기
       (value) {
@@ -230,7 +230,7 @@ class UserController extends GetxController {
       },
     );
 
-    /// 게시글에서 프로필 수정
+    // 게시글에서 프로필 수정
     _postDB
         .where('isDeleted', isEqualTo: false)
         .where('uid', isEqualTo: CurrentUser.uid)
@@ -260,7 +260,7 @@ class UserController extends GetxController {
       },
     );
 
-    /// 게임후기의 프로필 수정
+    // 게임후기의 프로필 수정
     _reviewDB.where('idFrom', isEqualTo: CurrentUser.uid).get().then(
       // 내가 보낸 게임후기만 쿼리하여 리스트로 받기
       (value) {
@@ -284,14 +284,14 @@ class UserController extends GetxController {
       },
     );
 
-    /// Auth의 프로필 수정
+    // Auth의 프로필 수정
     _auth.currentUser!.updatePhotoURL(profileUrl).then(
           (_) => print('auth의 프로필 수정'),
           onError: (e) => print(e),
         );
   }
 
-  /// 폰으로 SMS 전송
+  // 폰으로 SMS 전송
   Future verifyPhone(String phonenumber) async {
     return _auth.verifyPhoneNumber(
       // 폰번호
@@ -326,7 +326,7 @@ class UserController extends GetxController {
     );
   }
 
-  /// 휴대폰 번호로 신규 가입하기
+  // 휴대폰 번호로 신규 가입하기
   Future signUP(token) async {
     try {
       final credential = PhoneAuthProvider.credential(
@@ -343,21 +343,25 @@ class UserController extends GetxController {
     }
   }
 
-  /// 로그아웃
-  /// Db, Auth 정보 삭제 X
-  /// 탈퇴와 다르게 자동로그인 쿠키만 앱에서 지움
+  // 로그아웃
   Future signOut() async {
+    // 로그아웃시 푸시알림 수신 못하게 하기 위해 토큰값 null로 업데이트
+    await _userDB.doc(CurrentUser.uid).update(
+      {
+        'pushToken': null,
+      },
+    );
+    // 유저정보 토큰값을 앱 로컬에서 지우기
     _auth.signOut();
-    print('로그아웃');
   }
 
-  /// 탈퇴하기
+  // 탈퇴하기
   Future deleteUser(smsCode) async {
     try {
       final credential = await PhoneAuthProvider.credential(
           verificationId: verificationID, smsCode: smsCode);
 
-      /// 게시글 플래그
+      // 게시글 플래그
       _postDB
           .where('uid', isEqualTo: CurrentUser.uid)
           .where('isDeleted', isEqualTo: false)
@@ -387,7 +391,7 @@ class UserController extends GetxController {
         },
       );
 
-      /// 채팅 플래그
+      // 채팅 플래그
       _chatDB
           .where('members', arrayContains: CurrentUser.uid)
           // 채팅방 나가기 플래그가 false로 되어있다는 쿼리 추가하기 > 나중에
@@ -417,7 +421,7 @@ class UserController extends GetxController {
         onError: (e) => print(e),
       );
 
-      /// 게임후기 기본 프로필로 수정
+      // 게임후기 기본 프로필로 수정
       _reviewDB.where('idFrom', isEqualTo: CurrentUser.uid).get().then(
         // 내가 보낸 게임후기만 쿼리하여 리스트로 받기
         (value) {
@@ -441,8 +445,8 @@ class UserController extends GetxController {
         },
       );
 
-      /// 유저정보 처리
-      /// 탈퇴 플래그 true, 탈퇴 시간 업데이트
+      // 유저정보 처리
+      // 탈퇴 플래그 true, 탈퇴 시간 업데이트
       _userDB.doc(CurrentUser.uid).update(
         {
           'isWithdrawn': true,
@@ -453,27 +457,27 @@ class UserController extends GetxController {
         onError: (e) => print(e),
       );
 
-      /// Storage에서 해당 유저의 프로필 삭제
+      // Storage에서 해당 유저의 프로필 삭제
       // FirebaseStorage.instance.ref().child(CurrentUser.uid).delete();
 
-      /// 사용자 재인증
+      // 사용자 재인증
       await _auth.currentUser!.reauthenticateWithCredential(credential);
 
-      /// 장치에 저장된 유저 토큰 삭제
+      // 장치에 저장된 유저 토큰 삭제
       _auth.currentUser!.delete();
 
       print('탈퇴 성공');
     } on FirebaseAuthException catch (e) {
       if (e.code == 'invalid-verification-code') {
-        /// SMS 코드가 틀린 경우
-        /// 스낵바로 유저에게 알림
+        // SMS 코드가 틀린 경우
+        // 스낵바로 유저에게 알림
         Get.snackbar('인증코드 입력 오류', '입력한 인증 코드를 확인해주세요.');
       } else
         print(e);
     }
   }
 
-  /* uid를 통해 특정 유저의 정보 받기 */
+  // uid를 통해 특정 유저의 정보 받기
   Future getUserInfoByUid(uid) async {
     return _userDB.doc(uid).get().then(
       (value) {
