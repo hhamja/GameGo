fa7a8cc731bf044ffd42c7a8d34dd8a4b4d0184e
CodeKinggggggg <kmsaa@naver.com>
2023-02-01 20:25:39 +0900

    Issue - 매너나이의 변동
    ##DECS
    - 이슈내용
    : 매너나이가 -+0.1값이 변동하여야하는데 0.0000000001 으로 변동하는 문제

--- lib/controller/mannerAge/mannerAge.dart
+++ lib/controller/mannerAge/mannerAge.dart
@@ -7,10 +7,10 @@ class MannerAgeController extends GetxController {
   // double _increment = 0.1;
   // double _decrement = -0.1;
 
-  /* 매너나이 증가 시키는 함수 */
+  // 매너나이 증가 시키는 함수
   Future plusMannerAge(uid) async {
-    final double _increment = Decimal.parse('0.1').toDouble();
-    //유저 데이터의 매너나이 +0.1
+    final double _increment = Decimal.parse('1/10').toDouble();
+    // 유저 데이터의 매너나이 +0.1
     await _userDB.doc(uid).update(
       {
         'mannerAge': FieldValue.increment(_increment),
@@ -18,10 +18,10 @@ class MannerAgeController extends GetxController {
     );
   }
 
-  /* 매너나이 감소 시키는 함수 */
+  // 매너나이 감소 시키는 함수
   Future minusMannerAge(uid) async {
-    final double _decrement = Decimal.parse('-0.1').toDouble();
-    //유저 데이터의 매너나이 -0.1
+    final double _decrement = Decimal.parse('-1/10').toDouble();
+    // 유저 데이터의 매너나이 -0.1
     await _userDB.doc(uid).update(
       {
         'mannerAge': FieldValue.increment(_decrement),
