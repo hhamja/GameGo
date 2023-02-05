9447399f8487dbfc77fdebada1b069b9a09aeed6
CodeKinggggggg <kmsaa@naver.com>
2023-02-05 18:22:43 +0900

    Edit - 배경색
    ##DECS
    : 눈이 아파서 배경색을 pure 흰색이 아닌 살짝 lightWhite로 수정

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -1,9 +1,16 @@
 import 'package:mannergamer/utilites/index/index.dart';
 
-// const Color appGreyColor = Color(0xFF333333);
-const Color appWhiteColor = Color(0xFFFFFFFF);
-const Color appBlackColor = Color(0xFF000000);
 // 0xfff7313f : 여기어때
 // 0xffff0057 : 탈잉
 const Color appPrimaryColor = Color(0xffff0057);
-const Color appBackgroudColor = Color(0xFFFFFFFF);
+// 블랙
+const Color appBlackColor = Color(0xFF000000);
+const Color appLightBlack = Color(0xFF333333);
+// 그레이
+const Color appGrayColor = Colors.grey;
+const Color appLightGray = Colors.grey;
+// 흰색
+const Color appWhiteColor = Color(0xFFFFFFFF);
+const Color appLightWhite = Color(0xFFf5f5f7);
+
+const Color appBackgroudColor = appLightWhite;
