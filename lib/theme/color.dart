93293135b19443f05548ae0cccdbf2fbe5cee5f4
CodeKinggggggg <kmsaa@naver.com>
2023-02-06 14:30:03 +0900

    Add - color
    ##DECS
    : 앱 전체적으로 사용할 색상 데이터 추가
    : 메인색을 보조할 세컨드 색, 블루색,, 빨간색 추가

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -3,6 +3,9 @@ import 'package:mannergamer/utilites/index/index.dart';
 // 0xfff7313f : 여기어때
 // 0xffff0057 : 탈잉
 const Color appPrimaryColor = Color(0xffff0057);
+// const Color appSecondColor = Color(0xffFF82A2);
+const Color appSecondColor = Color(0xffFF82A2);
+
 // 블랙
 const Color appBlackColor = Color(0xFF000000);
 const Color appLightBlack = Color(0xFF333333);
@@ -12,5 +15,9 @@ const Color appLightGray = Colors.grey;
 // 흰색
 const Color appWhiteColor = Color(0xFFFFFFFF);
 const Color appLightWhite = Color(0xFFf5f5f7);
-
+// 파란색
+const Color appBlueColor = Colors.blue;
+// 빨강색 에러 텍스트 컬러
+const Color appRedColor = Colors.red;
+// 배경색
 const Color appBackgroudColor = appLightWhite;
