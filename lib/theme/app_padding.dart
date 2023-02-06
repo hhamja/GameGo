0f8a7f499d3619b9bd1b6983448589a0dbabbed9
CodeKinggggggg <kmsaa@naver.com>
2023-02-06 14:29:03 +0900

    Add - 앱 전체적으로 적용한 세로 간격
    ##DECS
    : 머터리얼 가이드에서의 8dp,16dp, 24dp 간격을 참고하여 설정
    : 대략적으로 dp를 px로 변경하였고 그와 유사한 간격을 테스팅하여 sp수치를 산출함
    : 스몰, 미디움, 라지의 높이 간격으로 나눔. 구체적으로 스몰 : 미디움 : 라지 = 1 : 2 : 3의 비율을 가짐

--- lib/theme/app_padding.dart
+++ lib/theme/app_padding.dart
@@ -1,6 +1,11 @@
 // 앱 화면의 전체적인 패딩값을 통일하기 위함
 import 'package:mannergamer/utilites/index/index.dart';
 
-class AppDefaultPadding {
-  static final padding = 15.sp;
+class AppSpaceData {
+  // 모든 스크린에 공통적으로 적용한 패딩 값
+  static final screenPadding = 15.sp;
+
+  static final heightLarge = 7.5.h;
+  static final heightMedium = 5.h;
+  static final heightSmall = 2.5.h;
 }
