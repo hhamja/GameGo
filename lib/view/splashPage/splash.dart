2f1e130e974ce30869ac786eefb667369e6ced7a
CodeKinggggggg <kmsaa@naver.com>
2023-02-06 11:50:18 +0900

    Edit - 스플래쉬 페이지 UI
    ##DECS
    : 스플래쉬 페이지의 앱 로고와 메인로고 페이지의 앱 로고 위치를 맞추기 위한 UI적인 수정

--- lib/view/splashPage/splash.dart
+++ lib/view/splashPage/splash.dart
@@ -5,21 +5,30 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-// initial 컨트롤러 바인딩 -> 1.5초뒤 유저정보에 따라 페이지 이동
-
+    // initial 컨트롤러 바인딩 -> 1.5초뒤 유저정보에 따라 페이지 이동
     return WillPopScope(
       // 해당 위젯은 취소키 방지 역할
       onWillPop: () async => false,
       child: Scaffold(
-        backgroundColor: appWhiteColor,
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(0),
-            child: Image.asset(
-              'assets/main_logo.png',
-              color: appPrimaryColor,
-              width: 40.w,
-              height: 40.h,
+        body: Container(
+          color: appWhiteColor,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(35.sp),
+                  child: Image.asset(
+                    'assets/main_logo.png',
+                    color: appPrimaryColor,
+                    width: 40.w,
+                    height: 40.h,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.84.h,
+                ),
+              ],
             ),
           ),
         ),
