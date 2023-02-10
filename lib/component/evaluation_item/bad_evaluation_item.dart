67c4e6e5f1918aed44bb19462fcde4fbfc15787a
CodeKinggggggg <kmsaa@naver.com>
2023-02-10 11:31:40 +0900

    Edit - 비매너평가아이템 컴포넌트 위젯의 UI
    ##DECS
    : 반응형 사이즈 적용
    : UI퀄리티 개선

--- lib/component/evaluation_item/bad_evaluation_item.dart
+++ lib/component/evaluation_item/bad_evaluation_item.dart
@@ -14,13 +14,24 @@ class BadEvaluationItem extends StatelessWidget {
     //동일 항목이 2개 이상인 경우에만 표시
     return element >= 2
         ? ListTile(
-            title: Text(title),
+            contentPadding: EdgeInsets.symmetric(
+              vertical: 10.sp,
+              horizontal: 0,
+            ),
+            title: Text(
+              title,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
             trailing: Row(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Icon(Icons.people_alt_outlined),
-                SizedBox(width: 5),
-                Text('$element'),
+                SizedBox(width: 4.sp),
+                // 받은 매너평가 항목 총 개수
+                Text(
+                  '$element',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
               ],
             ),
           )
