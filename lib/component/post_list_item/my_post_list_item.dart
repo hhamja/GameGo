17c7b3a35ef3ec5e61ae08a76b6ac486962d92ba
CodeKinggggggg <kmsaa@naver.com>
2023-02-12 13:40:34 +0900

    Edit - 나의 게시글 리스트 페이지 UI
    ##DECS
    : 반응형 사이즈 적용
    : UI 퀄리티 높이는 방향으로 수정

--- lib/component/post_list_item/my_post_list_item.dart
+++ lib/component/post_list_item/my_post_list_item.dart
@@ -2,7 +2,6 @@ import 'package:mannergamer/utilites/index/index.dart';
 
 class CustomMyPostListItem extends StatelessWidget {
   CustomMyPostListItem(
-    this.profileUrl,
     this.title,
     this.gamemode,
     this.position,
@@ -11,7 +10,6 @@ class CustomMyPostListItem extends StatelessWidget {
     this.onTap,
   );
 
-  final String profileUrl;
   final String title;
   final String gamemode;
   final String? position;
@@ -23,18 +21,18 @@ class CustomMyPostListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     // 제목 텍스트 스타일
     final TextStyle _titleTextStyle = TextStyle(
-      fontSize: 12.sp,
-      letterSpacing: Theme.of(context).textTheme.labelLarge!.letterSpacing,
-      fontWeight: FontWeight.bold,
-      color: Theme.of(context).textTheme.bodyMedium!.color,
-      overflow: TextOverflow.ellipsis,
-    );
-    // 서브 텍스트 스타일
-    final TextStyle _subTextStyle = TextStyle(
       fontSize: Theme.of(context).textTheme.bodyMedium!.fontSize,
       letterSpacing: Theme.of(context).textTheme.bodyMedium!.letterSpacing,
       fontWeight: Theme.of(context).textTheme.bodyMedium!.fontWeight,
-      color: appBlackColor,
+      color: Theme.of(context).textTheme.bodyMedium!.color,
+      overflow: TextOverflow.ellipsis,
+    );
+    // 제목 아래의 서브 텍스트
+    final TextStyle _subTextStyle = TextStyle(
+      fontSize: Theme.of(context).textTheme.bodySmall!.fontSize,
+      letterSpacing: Theme.of(context).textTheme.bodySmall!.letterSpacing,
+      fontWeight: Theme.of(context).textTheme.bodySmall!.fontWeight,
+      color: appGreyColor,
     );
     return InkWell(
       onTap: onTap,
@@ -43,67 +41,53 @@ class CustomMyPostListItem extends StatelessWidget {
           vertical: AppSpaceData.screenPadding,
         ),
         child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              height: 30.sp,
-              width: 30.sp,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(
-                  15.sp,
-                ),
-                child: Image.network(
-                  profileUrl,
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
             Expanded(
-              child: Padding(
-                padding: EdgeInsets.only(
-                  left: AppSpaceData.screenPadding * 0.8,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    // 제목 또는 닉네임
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          title,
-                          maxLines: 1,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // 제목 또는 닉네임
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          '내일 저녁에 같이 게임할 듀오 여성분 구합니다 많은 관심 부탁드려요.',
+                          maxLines: 2,
                           style: _titleTextStyle,
-                        ), // 글 시간
-                        Text(
-                          '·${time}',
-                          style: TextStyle(
-                            fontSize: 10.sp,
-                            color: Colors.grey,
-                          ),
+                          overflow: TextOverflow.ellipsis,
                         ),
-                      ],
-                    ),
-                    SizedBox(height: 1.sp),
-                    // 게임모드 · 포지션 · 티어
-                    Row(
-                      children: [
-                        Text(
-                          gamemode,
-                          style: _subTextStyle,
-                        ),
-                        Text(
-                          position != null ? '·${position}' : '',
-                          style: _subTextStyle,
-                        ),
-                        Text(
-                          tear != null ? '·${tear}' : '',
-                          style: _subTextStyle,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 5.sp),
+                  // 게임모드 · 포지션 · 티어
+                  Row(
+                    children: [
+                      Text(
+                        gamemode,
+                        style: _subTextStyle,
+                      ),
+                      Text(
+                        position != null ? ' ·${position}' : '',
+                        style: _subTextStyle,
+                      ),
+                      Text(
+                        tear != null ? '·${tear}' : '',
+                        style: _subTextStyle,
+                      ),
+                      // 글 시간
+                      Text(
+                        '·${time}',
+                        style: _subTextStyle,
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
--- lib/view/my_info/my_activity/my_post/my_post_list.dart
+++ lib/view/my_info/my_activity/my_post/my_post_list.dart
@@ -8,53 +8,47 @@ class MyPostListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Text(
           '나의 글',
-          style: Theme.of(context).textTheme.titleMedium,
+          style: Theme.of(context).textTheme.titleSmall,
         ),
-        centerTitle: true,
+        actions: [
+          CustomCloseButton(),
+        ],
       ),
       body: Obx(
-        () => RefreshIndicator(
-          // 새로고침 시 PostList의 바뀐 값을 반영하여 Ui에 업데이트함
-          onRefresh: () async {
-            await _controller.getMyPostList();
-          },
-          // 맨 위에 위치시키는 값
-          displacement: 0,
-          child: ListView.separated(
-              // 리스트가 적어도 스크롤 인식 가능
-              physics: AlwaysScrollableScrollPhysics(),
-              itemBuilder: (BuildContext context, int index) {
-                final _profileUrl = _controller.myPostList[index].profileUrl;
-                final _title = _controller.myPostList[index].title;
-                final _gamemode = _controller.myPostList[index].gamemode;
-                final _position = _controller.myPostList[index].position;
-                final _tear = _controller.myPostList[index].tear;
-                // 게시글 생성시간 '-전'으로 표시
-                String _time =
-                    Jiffy(_controller.myPostList[index].updatedAt.toDate())
-                        .fromNow();
-                _onTap() {
-                  Get.toNamed('/postdetail', arguments: {
-                    'postId': _controller.myPostList[index].postId,
-                  });
-                }
+        () => ListView.builder(
+          padding: EdgeInsets.symmetric(
+            horizontal: AppSpaceData.screenPadding,
+          ),
+          physics: AlwaysScrollableScrollPhysics(),
+          itemBuilder: (BuildContext context, int index) {
+            final String _title = _controller.myPostList[index].title;
+            final String _gamemode = _controller.myPostList[index].gamemode;
+            final String? _position = _controller.myPostList[index].position;
+            final String? _tear = _controller.myPostList[index].tear;
+            final Timestamp _updatedAt =
+                _controller.myPostList[index].updatedAt;
+            // 게시글 생성시간 '-전'으로 표시
+            final String _time = Jiffy(_updatedAt.toDate()).fromNow();
+            _onTap() {
+              Get.toNamed(
+                '/postdetail',
+                arguments: {'postId': _controller.myPostList[index].postId},
+              );
+            }
 
-                return CustomMyPostListItem(
-                  _profileUrl,
-                  _title,
-                  _gamemode,
-                  _position,
-                  _tear,
-                  _time,
-                  _onTap,
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) {
-                return CustomDivider();
-              },
-              itemCount: _controller.myPostList.length),
+            return CustomMyPostListItem(
+              _title,
+              _gamemode,
+              _position,
+              _tear,
+              _time,
+              _onTap,
+            );
+          },
+          itemCount: _controller.myPostList.length,
         ),
       ),
     );
